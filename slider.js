6ebc962cd9b2549fbb85fcbd1c8119725877f7cd
Zeynep Afat <75540856+zey19@users.noreply.github.com>
2022-05-24 18:40:47 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	
+        	$('.slider').bxSlider({
+			  auto: true,
+			  minSlides: 1,
+			  maxSlides: 1,
+			  moveSlides:1,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			 randomStart: true,
+			 default:3000,
+			 pager: true,
+			 pagerType:'short',
+
+			
+			});
+		
+    	});
